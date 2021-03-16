c5d019b992a5279c2511a10742fc1afc4ce63106
semin <semin015123@gmail.com>
2021-03-16 10:38:18 +0900

    set drawer and several Activity Complete & Need to set Custom Calender

--- app/src/main/java/com/example/wgout/HomeActivity.java
+++ app/src/main/java/com/example/wgout/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,44 +12,93 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HomeActivity extends AppCompatActivity {
-    private Button btn_test;
-    private Button btn_close;
-    private TextView tv_test;
+    private Button btn_drawer;
+    private Button btn_calender;
+    private Button btn_destination;
+    private Button btn_luck;
+    private Button btn_weather;
+
+    private TextView tv_home_date;
 
     private DrawerLayout drawerLayout;
     private View drawerView;
+    private String strDate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        tv_test = (TextView)findViewById(R.id.tv_test);
-        btn_test = (Button)findViewById(R.id.btn_test);
-        btn_close = (Button)findViewById(R.id.btn_close);
+
+        Intent intent = getIntent();
+
+        init_view();
+        btn_clicked();
+        //drawerLayout.addDrawerListener(listener);
+
+        strDate = new SimpleDateFormat("yyyy.MM.dd").format(new Date(System.currentTimeMillis()));
+        tv_home_date.setText(strDate);
+
+    }
+
+    private void init_view(){
+        btn_drawer = (Button)findViewById(R.id.btn_drawer);
+        btn_calender = (Button)findViewById(R.id.btn_calender);
+        btn_destination = (Button)findViewById(R.id.btn_destination);
+        btn_luck = (Button)findViewById(R.id.btn_luck);
+        btn_weather = (Button)findViewById(R.id.btn_weather);
+
+        tv_home_date = (TextView)findViewById(R.id.tv_home_date);
 
         drawerLayout = (DrawerLayout)findViewById(R.id.activity_home);
         drawerView = (View)findViewById(R.id.drawer);
+    }
 
-        Intent intent = getIntent();
-        btn_test.setOnClickListener(new View.OnClickListener() {
+    private void btn_clicked(){
+        btn_drawer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(drawerView);
             }
         });
 
-        btn_close.setOnClickListener(new View.OnClickListener() {
+        btn_calender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.closeDrawers();
+                Intent intent = new Intent(HomeActivity.this, CalenderActivity.class);
+                startActivity(intent);
             }
         });
-
-        drawerLayout.addDrawerListener(listener);
-
+        btn_destination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+                Intent intent = new Intent(HomeActivity.this, DestinationActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_luck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+                Intent intent = new Intent(HomeActivity.this, LuckActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+                Intent intent = new Intent(HomeActivity.this, WeatherActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
+/*
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
         @Override
         public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
@@ -70,4 +120,5 @@ public class HomeActivity extends AppCompatActivity {
 
         }
     };
+ */
 }
--- app/src/main/java/com/example/wgout/MainActivity.java
+++ app/src/main/java/com/example/wgout/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
-
             }
         });
     }
